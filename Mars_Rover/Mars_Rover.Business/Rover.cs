e09a0219c49d8f0c2255937a726cc6bbe781133e
Adriel Mariappen <adriel@kironinteractive.com>
2021-02-24 07:55:12 +0200

    Intial commit

---
+++ Mars_Rover/Mars_Rover.Business/Rover.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mars_Rover.Business
+{
+    public class Rover
+    {
+        public int x;  //x coordinate
+        public int y;  //y coordintate
+        public string Direction; //cardinal direction
+
+        public Rover(string _location) //_location = {1 2 N}
+        {
+            Int32.TryParse(_location.Split(" ")[0], out x); // pick out string input of x
+            Int32.TryParse(_location.Split(" ")[1], out y); //pick out string input of y
+            Direction = _location.Split(" ")[2];  // pick out string input of direction
+        }
+
+
+        public void SpinLeft()
+        {
+            switch (Direction)
+            {
+                case "N": //Rover spin left N => W
+                    Direction = "W";
+                    break;
+                case "E": //Rover spin left E => N
+                    Direction = "N";
+                    break;
+                case "S": //Rover spin left S => E
+                    Direction = "E";
+                    break;
+                case "W": //Rover spin left W => S
+                    Direction = "S";
+                    break;
+                default:
+                    throw new Exception("Direction is not valid");
+            }
+        }
+
+        public void SpinRight()
+        {
+            switch (Direction)
+            {
+                case "N": //Rover spin right N => E
+                    Direction = "E";
+                    break;
+                case "E": //Rover spin right E => S
+                    Direction = "S";
+                    break;
+                case "S": //Rover spin right S => W
+                    Direction = "W";
+                    break;
+                case "W": //Rover spin right W => N
+                    Direction = "N";
+                    break;
+                default:
+                    throw new Exception("Direction is not valid");
+            }
+        }
+
+        
+        public void MoveForward()
+        {
+            switch (Direction)
+            {
+                case "N": //Rover move forward N => y+1
+                    y += 1;
+                    break;
+                case "E": //Rover move forward E => x+1
+                    x += 1;
+                    break;
+                case "S": //Rover move forward S => y-1
+                    y -= 1;
+                    break;
+                case "W": //Rover move forward W => x-1
+                    x -= 1;
+                    break;
+                default:
+                    throw new Exception("Direction is not valid");
+            }
+        }
+
+        public void GoToLocation(string roverCommand)
+        {
+            //Get instructions from string provided.
+            char[] intructions = roverCommand.ToCharArray();
+
+            //Iterate through array of string for each char, then call SpinLeft,SpinRight,MoveForward based on intruction.
+            for(int i=0;i<intructions.Length;i++)
+            {
+                switch (intructions[i].ToString())
+                {
+                    case "L": //When instructed to move Left
+                        SpinLeft();
+                        break;
+                    case "R":  //When instructed to move Right
+                        SpinRight();
+                        break;
+                    case "M":  //When instructed to move Move Forward
+                        MoveForward();
+                        break;
+                    default:
+                        throw new Exception("Instruction is not valid");
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Mars_Rover/Mars_Rover.Test/RoverShould.cs
@@ -0,0 +1,100 @@
+using Mars_Rover.Business;
+using System;
+using Xunit;
+
+namespace Mars_Rover.Test
+{
+    public class RoverShould
+    {
+        [Fact]
+        public void SpinLeft()
+        {
+            #region Test Case 1: Input {1 2 N} to Spin Left => Return Direction {W}
+            //Arrange
+            Rover _rover = new Rover("1 2 N");
+
+            //Act
+            _rover.SpinLeft();
+
+            //Assert
+            Assert.Equal("W", _rover.Direction);
+            #endregion
+
+            #region Test Case 2: Input {1 2 E} to Spin Left => Return Direction {N}
+            //Arrange
+            _rover = new Rover("1 2 E");
+
+            //Act
+            _rover.SpinLeft();
+
+            //Assert
+            Assert.Equal("N", _rover.Direction);
+            #endregion
+
+            #region Test Case 3: Input {1 2 S} to Spin Left => Return Direction {E}
+            //Arrange
+            _rover = new Rover("1 2 S");
+
+            //Act
+            _rover.SpinLeft();
+
+            //Assert
+            Assert.Equal("E", _rover.Direction);
+            #endregion
+
+            #region Test Case 4: Input {1 2 W} to Spin Left => Return Direction {S}
+            //Arrange
+            _rover = new Rover("1 2 W");
+
+            //Act
+            _rover.SpinLeft();
+
+            //Assert
+            Assert.Equal("S", _rover.Direction);
+            #endregion
+        }
+
+        [Fact]
+        public void SpinRight()
+        {
+            //Arrange
+            Rover _rover = new Rover("1 2 N");
+
+            //Act
+            _rover.SpinRight();
+
+
+            //Assert
+            Assert.Equal("E", _rover.Direction);
+        }
+
+        [Fact]
+        public void MoveForward()
+        {
+            //Arrange
+            Rover _rover = new Rover("1 2 N");
+
+            //Act
+            _rover.MoveForward();
+
+
+            //Assert
+            Assert.Equal(3, _rover.y);
+        }
+
+
+        [Fact]
+        public void DestinationPoint()
+        {
+            //Arrange
+            Rover _rover = new Rover("1 2 N");
+
+            //Act
+            _rover.GoToLocation("LMLMLMLMM");
+
+
+            //Assert
+            Assert.Equal("1 3 N", _rover.x + " " + _rover.y + " " + _rover.Direction);
+        }
+    }
+}
